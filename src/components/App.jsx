e5a68aca65ee36054388b7d00f60f85e3d90baa7
MachDomo <dominic.c.ma@gmail.com>
2017-09-08 16:25:39 -0700

    "Implemented click handler"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,38 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedVideo: window.exampleVideoData[0],
+      videos: window.exampleVideoData
+    };
+
+    this.handleVideoClick = this.handleVideoClick.bind(this);
+  }
+
+  handleVideoClick(selectedVideo) {
+    this.setState({selectedVideo});
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.selectedVideo} />
+          </div>
+          <div className="col-md-5">
+            <VideoList handler={this.handleVideoClick} videos={this.state.videos} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={window.exampleVideoData[0]} />
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={window.exampleVideoData} />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: options,
+    success: callback,
+    error: (err)=> console.log(err)
+  });
 };
 
 window.searchYouTube = searchYouTube;
+
+
+/*
+options = {
+q:'searchterm',
+max:5,
+key: youtubekey,
+embeddableVideos:true,
+part:'snippet'
+}
+*/
